83ad055ce06069166d5af41a3ade245fbb7e961b
Thiago Programador <60783280+univer30@users.noreply.github.com>
2022-03-22 10:45:56 -0300

    Nova atualização
    Finalização do projeto

--- ProjetoHtml/js/scripts.js
+++ ProjetoHtml/js/scripts.js
@@ -4,11 +4,43 @@ function logo(){
 }
 
 function login(){
-    
+   
     document.getElementById('modal').style.display="flex";
+    document.getElementById('carousel').style.opacity="0.9";
+   
 }
 
 function loginEnd(){
     document.getElementById('modal').style.display="none";
+    document.getElementById('carousel').style.opacity="1";
+}
+
+
+
+function cep(){
+    var cepReturn = document.getElementById('c');
+
+    var linkViacep = "https://viacep.com.br/ws/"+cepReturn.value+"/json/";
     
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", linkViacep, true);
+    xhr.onload = function(e) {
+       if(xhr.readyState === 4){
+           if(xhr.status === 200){
+               var dados = JSON.parse(xhr.responseText);
+               console.log("Sucesso");
+               document.getElementById("end").value = dados.logradouro;
+               document.getElementById("bairro").value = dados.bairro;
+               document.getElementById("cidade").value = dados.localidade;
+            }else{
+              console.error(xhr.statusText);
+           }
+       }
+    };
+    xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+  };
+
+  xhr.send();
+
 }
