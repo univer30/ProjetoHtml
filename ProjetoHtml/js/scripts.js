2f255d8120f0200028460860c1e5052dc843ffe9
Thiago Programador <60783280+univer30@users.noreply.github.com>
2022-03-22 10:00:33 -0300

    Nova atualização do projeto
    Atualização do projeto criação de um modal de cadastro e login

--- ProjetoHtml/js/scripts.js
+++ ProjetoHtml/js/scripts.js
@@ -2,3 +2,13 @@
 function logo(){
     window.location.href="projeto.html";
 }
+
+function login(){
+    
+    document.getElementById('modal').style.display="flex";
+}
+
+function loginEnd(){
+    document.getElementById('modal').style.display="none";
+    
+}
